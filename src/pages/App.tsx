ea5bcb6489569d3e096b11974d58f97f66250306
Serban Iacob <iacobserban17danieloff@gmail.com>
2025-04-20 13:25:48 +0300

    changed padding + finished the query type

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
           <h2 className="mb-2 card-title text-3xl text-[var(--color-grey-900)]">
             Contact Us
           </h2>
-          <fieldset className="fieldset rounded-box flex flex-col w-full gap-2">
+          <fieldset className="fieldset flex flex-col w-full gap-2">
             <div className="flex flex-row justify-between gap-5">
               <div className="w-full">
                 <label className="label text-lg mb-1 text-[var(--color-grey-900)]">
@@ -19,13 +19,15 @@ export default function App() {
                 </label>
                 <input
                   type="input"
-                  className="input validator w-full text-xl text-[var(--color-grey-900)]
+                  className="input validator w-full text-xl p-6 text-[var(--color-grey-900)]
                   focus:outline-0 focus:ring-1 focus:ring-[var(--color-green-600)]
                   border-[var(--color-grey-500)] rounded-lg
                   user-invalid:border-[var(--color-red)] user-invalid:focus:ring-[var(--color-red)]"
                   required
                 />
-                <p className="validator-hint text-sm">This field is required</p>
+                <p className="validator-hint text-sm -mb-2">
+                  This field is required
+                </p>
               </div>
 
               <div className="w-full">
@@ -37,13 +39,15 @@ export default function App() {
                 </label>
                 <input
                   type="input"
-                  className="input validator w-full text-xl text-[var(--color-grey-900)]
+                  className="input validator w-full text-xl p-6 text-[var(--color-grey-900)]
                   focus:outline-0 focus:ring-1 focus:ring-[var(--color-green-600)]
                   border-[var(--color-grey-500)] rounded-lg
                   user-invalid:border-[var(--color-red)] user-invalid:focus:ring-[var(--color-red)]"
                   required
                 />
-                <p className="validator-hint text-sm">This field is required</p>
+                <p className="validator-hint text-sm -mb-2">
+                  This field is required
+                </p>
               </div>
             </div>
 
@@ -55,15 +59,61 @@ export default function App() {
               <input
                 type="email"
                 pattern=".*@.*\.com"
-                className="input validator w-full text-xl text-[var(--color-grey-900)]
+                className="input validator w-full text-xl p-6 text-[var(--color-grey-900)]
                   focus:outline-0 focus:ring-1 focus:ring-[var(--color-green-600)]
                   border-[var(--color-grey-500)] rounded-lg
                   user-invalid:border-[var(--color-red)] user-invalid:focus:ring-[var(--color-red)]"
                 required
               />
-              <p className="validator-hint text-sm">
+              <p className="validator-hint text-sm -mb-2">
                 Please enter a valid email address
               </p>
+            </div>
+
+            <div>
+              <label className="label text-lg mb-1 text-[var(--color-grey-900)]">
+                Query Type{" "}
+                <span className="text-[var(--color-green-600)] text-xl">*</span>
+              </label>
+              <div className="flex gap-4">
+                <label
+                  className="validator flex items-center gap-4 p-2 w-1/2 border rounded-lg cursor-pointer 
+                  text-xl text-[var(--color-grey-900)] 
+                border-[var(--color-grey-500)] 
+                  focus-within:outline-0 focus-within:ring-1 focus-within:ring-[var(--color-green-600)] 
+                user-invalid:border-[var(--color-red)] user-invalid:focus-within:ring-[var(--color-red)]"
+                >
+                  <input
+                    type="radio"
+                    name="queryType"
+                    value="general"
+                    className="radio radio-sm ml-5"
+                    required
+                  />
+                  <span>General Enquiry</span>
+                </label>
+
+                <label
+                  className="validator flex items-center p-2 gap-2 w-1/2 border rounded-lg cursor-pointer 
+                  text-xl text-[var(--color-grey-900)] 
+                border-[var(--color-grey-500)] 
+                  focus-within:outline-0 focus-within:ring-1 focus-within:ring-[var(--color-green-600)] 
+                user-invalid:border-[var(--color-red)] user-invalid:focus-within:ring-[var(--color-red)]"
+                >
+                  <input
+                    type="radio"
+                    name="queryType"
+                    value="support"
+                    className="radio radio-sm ml-5"
+                  />
+                  <span>Support Request</span>
+                </label>
+              </div>
+
+              {/* <p className="validator-hint text-sm -mb-2">
+                Please select a query type
+              </p> */}
+              {/* I'm having issue with the validator hint above :( Idk how to fix it*/}
             </div>
           </fieldset>
           <div className="card-actions justify-center items-center h-12">
